61017ab021fefbc90951d696b7fa95a4ae633e0d
SreejaJogu <153176803+SreejaJogu@users.noreply.github.com>
2024-02-15 13:29:46 +0530

    Update question2.js

--- Coding challenges Assignment/question2.js
+++ Coding challenges Assignment/question2.js
@@ -2,8 +2,3 @@ console.log(multiplyNumbers(3, 4));//ReferenceError : Cannot acess 'multiplyNumb
 const multiplyNumbers = function (num1, num2){
     return num1 * num2;
 }
-
-/*console.log(addNumbers(5, 10));//15
-function addNumbers(num1, num2){
-    return num1 * num2;
-}*/
